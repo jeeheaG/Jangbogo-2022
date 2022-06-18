6e1d7ceed444b9919d5ae9538aefae90735d43f9
jeeheaG <jeehea99@naver.com>
2022-06-18 15:38:15 +0900

    [FEAT] Make ProductAddActivity view

--- app/src/main/java/com/example/jangbogo_2022/ProductAddActivity.kt
+++ app/src/main/java/com/example/jangbogo_2022/ProductAddActivity.kt
@@ -2,6 +2,7 @@ package com.example.jangbogo_2022
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.jangbogo_2022.databinding.ActivityProductAddBinding
 
 class ProductAddActivity : AppCompatActivity() {
@@ -16,7 +17,11 @@ class ProductAddActivity : AppCompatActivity() {
             finish()
         }
 
-
+        binding.btnProductAdd.setOnClickListener {
+        //TODO : 서버에 상품추가 요청
+            Toast.makeText(this, "상품을 추가합니다.", Toast.LENGTH_SHORT).show()
+            finish()
+        }
 
     }
 }
